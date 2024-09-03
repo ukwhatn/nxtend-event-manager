f9b4f5a4e06f0dc89832bcdefa476ee44346e037
Yuki Watanabe <ukwhatn@gmail.com>
2024-09-03 18:56:20 +0900

    add: user model definition

--- db/migrate/20240903094532_create_users.rb
+++ db/migrate/20240903094532_create_users.rb
@@ -1,6 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
+      t.bigint :discord_id, null: false
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :email, null: false
+      t.string :school_name, null: true
+      t.integer :graduation_year, null: true
+      t.string :circle_name, null: true
       t.timestamps
     end
   end
