83564c38b5692dd3db791c0f0c261f8fc2a12bdf
Yuki Watanabe <ukwhatn@gmail.com>
2024-09-03 18:57:31 +0900

    add: userlogintoken model def

--- db/migrate/20240903094603_create_user_login_tokens.rb
+++ db/migrate/20240903094603_create_user_login_tokens.rb
@@ -1,6 +1,10 @@
 class CreateUserLoginTokens < ActiveRecord::Migration[7.2]
   def change
     create_table :user_login_tokens do |t|
+      t.bigint :discord_id, null: false
+      t.string :token, null: false
+      t.datetime :expired_at, null: false
+      t.boolean :is_used, null: false, default: false
       t.timestamps
     end
   end
