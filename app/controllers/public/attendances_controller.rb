5bc24a02c9438f832f5b584d93d3be47c1a8bc85
Yuki Watanabe <ukwhatn@gmail.com>
2024-09-06 01:11:46 +0900

    wip

--- app/controllers/public/attendances_controller.rb
+++ app/controllers/public/attendances_controller.rb
@@ -1,6 +1,8 @@
 class Public::AttendancesController < ApplicationController
   before_action :check_logged_in
 
+  include ActionView::Helpers::DateHelper
+
   def create
 
     token = params[:token]
@@ -14,13 +16,40 @@ class Public::AttendancesController < ApplicationController
     end
 
     if event.present?
+      event_name = "イベント：#{event.name}"
+      # 存在チェック
+      if @user.events.include?(event)
+        render template: "public/attendances/error", locals: {
+          message: "既に登録されています",
+          description1: event_name,
+          description2: "登録日時：#{time_ago_in_words(@user.user_event_attendances.find_by(event_id: event.id).created_at)}前"
+        }
+        return
+      end
+
       attendance = @user.user_event_attendances.new(event_id: event.id)
     else
       event = program.event
 
       # ユーザがイベントに参加していない場合はエラー
       unless @user.events.include?(event)
-        redirect_to root_path, alert: "イベントに出席登録していません、スタッフにお声がけください"
+        render template: "public/attendances/error", locals: {
+          message: "イベントに参加していません",
+          description1: "スタッフにお声がけください",
+          description2: "イベント：#{event.name}"
+        }
+        return
+      end
+
+      event_name = "イベント：[#{event.name}] #{program.name}"
+
+      # 存在チェック
+      if @user.event_programs.include?(program)
+        render template: "public/attendances/error", locals: {
+          message: "既に登録されています",
+          description1: event_name,
+          description2: "登録日時：#{time_ago_in_words(@user.user_program_attendances.find_by(event_program_id: program.id).created_at)}前"
+        }
         return
       end
 
@@ -28,9 +57,16 @@ class Public::AttendancesController < ApplicationController
     end
 
     if attendance.save
-      redirect_to root_path, notice: "出席を記録しました"
+      render template: "public/attendances/first", locals: {
+        description1: event_name,
+        description2: "スタッフへの提示が必要な場合がありますので、画面はそのままにしてください"
+      }
     else
-      redirect_to root_path, alert: "出席の記録に失敗しました、再度お試しください"
+      render template: "public/attendances/error", locals: {
+        message: "出席登録時にエラーが発生しました",
+        description1: event_name,
+        description2: "スタッフにお声がけください"
+      }
     end
 
   end
