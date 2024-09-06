2b737b96e4333a3ff87cd12ef397df8e0569921e
Yuki Watanabe <ukwhatn@gmail.com>
2024-09-06 10:48:34 +0900

    デプロイ周り

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@ key = Rails.env.production? ? "_app_session" : "_app_session_#{Rails.env}"
 domain = ENV.fetch("APP_HOST", "localhost")
 
 NxtendEventManager::Application.config.session_store :redis_store,
-                                                servers: ["redis://localhost:6379/0/session"],
+                                                servers: ["redis://redis:6379/0/session"],
                                                 expire_after: 4.days,
                                                 key:,
                                                 domain:,
